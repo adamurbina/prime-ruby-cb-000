496a43b29e931282eaf2814ea5eaa739d89b111a
Adam Urbina <adamurbina@gmail.com>
2018-06-01 01:02:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime(number)
+
+    return false if number < 2
+
+    half = (number / 2).round
+    half.each { |div|
+        if number % div == 0
+            return true
+        end
+    }
+
+    false
+end
